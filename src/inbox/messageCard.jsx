ea98259fadaefc8f88e312ea7bf7defee77974ff
dallin-windows-hp <dallin-windows-hp>
2025-10-22 16:27:22 -0600

    JS for Inbox: Created MessageCard component

---
+++ src/inbox/messageCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './inbox.css';
+
+import plane from '../assets/paper-plane.png';
+
+export default function MessageCard() {
+    return (
+        <button className="btn w-100">
+            <div className="card d-flex flex-row justify-content-start">
+                <div className="d-flex flex-column p-3">
+                    <img className="img-fluid" src={plane} width="48"/>
+                    <div className="badge text-bg-secondary">NEW</div>
+                </div>
+                <div className="card-body">
+                    <h5 className="card-title"><strong>Friend's Name</strong></h5>
+                    <p className="card-text">Message preview...</p>
+                </div>
+            </div>
+        </button>
+    )
+}
