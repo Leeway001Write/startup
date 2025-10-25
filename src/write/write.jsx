3cbe561c43703e21d517d90f2af0cb837d02f0ca
dallin-windows-hp <dallin-windows-hp>
2025-10-25 01:01:10 -0600

    Added recipient field

--- src/write/write.jsx
+++ src/write/write.jsx
@@ -18,7 +18,13 @@ export default function Write() {
         <main className="d-flex flex-column justify-content-start align-items-center p-4 flex-fill">
             <h2>Write a letter</h2>
             <form>
-                <textarea id="letter" className="paper p-3" placeholder="Write your message here"></textarea>
+                <div className="paper p-0 d-flex flex-column">
+                    <div className="recipient-line p-0 m-0 d-flex align-items-center">
+                    <label className="writing p-0">Dear</label>
+                    <input type="text" className="content p-3" placeholder="recipient,"></input>
+                    </div>
+                    <textarea id="letter" className="content writing p-3" placeholder="Write your message here"></textarea>
+                </div>
                 <button type="button" className="btn btn-lg bg-secondary text-white" onClick={ sendHandler }>Send</button>
             </form>
         </main>
