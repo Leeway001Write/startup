f2a6d0b09100e272fc1a4028250eb6fc94a4db9e
dallin-windows-hp <dallin-windows-hp>
2025-10-29 17:41:11 -0600

    Added Delete All button to Inbox

--- src/inbox/inbox.jsx
+++ src/inbox/inbox.jsx
@@ -41,17 +41,34 @@ export default function Inbox() {
         }
     };
 
+    const handleDelete = function() {
+        if (confirm("Permanently DELETE all messages?")) {
+            // Clear DB for user
+            let user = localStorage.getItem('user');
+            let messageDB = JSON.parse(localStorage.getItem('messages'));
+            messageDB[user] = [];
+            localStorage.setItem('messages', JSON.stringify(messageDB));
+        
+            // Update page
+            setMessagesList([]);
+            setCurrentMessage(null);
+        }
+    }
+
     return (
         <main className="flex-fill d-flex flex-column justify-content-between align-items-center p-3">
             <h2>Inbox</h2>
 
-            <div className="window container-fluid d-flex gap-5 flex-fill">
-                <div className="messages border rounded flex-fill overflow-y-scroll">
+            <div id="window" className="container-fluid d-flex gap-5 flex-fill">
+                <div id="controls" className="d-flex flex-column align-items-end gap-1 m-0 p-0">
+                    <button id="delete-button" className="btn btn-small bg-warning text-white" onClick={ handleDelete }>Delete All</button>
+                    <div className="messages border rounded flex-fill overflow-y-scroll">
 
-                    { messagesList.map((msg, i) => (
-                        <MessageCard key={i} sender={msg.sender} content={msg.content} isUnread={msg.isUnread} onClick={ () => clickMessageHandler(i) } />
-                    ))}
+                        { messagesList.map((msg, i) => (
+                            <MessageCard key={i} sender={msg.sender} content={msg.content} isUnread={msg.isUnread} onClick={ () => clickMessageHandler(i) } />
+                        ))}
 
+                    </div>
                 </div>
 
                 <div className="view-message container-fluid p-5 bg-primary">
