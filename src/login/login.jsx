fff5b5c892ed01c102160ae3be1e79a043a3f5ef
dallin-windows-hp <dallin-windows-hp>
2025-10-29 20:18:02 -0600

    Weather API test

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -22,10 +22,26 @@ export default function Login() {
         loginUser(null);
     }
 
+    const getWeather = async function() {
+        const pos = await getPosition();
+        const lat = pos.coords.latitude;
+        const lon = pos.coords.longitude;
+
+        const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=is_day,weather_code&timezone=auto`);
+        console.log(response.json(), (new Date()).getHours());
+    }
+
     return (
         <main className="flex-fill d-flex flex-column justify-content-center align-items-center">
             {user == null && <Unauthenticated onLogin={ loginUser } />}
             {user != null && <Authenticated user={ user } onLogout={ logoutUser }/>}
+            <button className="btn" onClick={ getWeather }>GET WEATHER</button>
         </main>
     );
+}
+
+function getPosition() {
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
 }
