6f054b88490bc89a37a6ee95bcfc18f79b913499
dallin-windows-hp <dallin-windows-hp>
2025-10-30 02:39:44 -0600

    Fixed server unable to start properly

--- service/service.js
+++ service/service.js
@@ -1,3 +1,10 @@
+const express = require('express');
+const app = express();
+
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
 app.use(express.static('public'));
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
